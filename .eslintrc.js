60a798ea87f3ed6b349288bb09544b0f2379fe5f
jakubkoci <jakub.koci@gmail.com>
2018-07-24 23:47:31 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'plugin:prettier/recommended', 'prettier', 'prettier/react'],
+  plugins: ['prettier', 'react', 'jsx-a11y', 'import'],
+  rules: {
+    'no-use-before-define': 'off', // http://eslint.org/docs/rules/no-use-before-define because of top-bottom
+    'react/sort-comp': [
+      1,
+      {
+        order: ['static-methods', 'lifecycle', 'everything-else', 'render'],
+      },
+    ],
+    'import/extensions': ['error', { json: 'always', css: 'always' }],
+    'import/prefer-default-export': 'off',
+    'react/prop-types': 'off', // because of Flow types instead of prop-types
+    'react/jsx-filename-extension': 'off',
+  },
+}
---
+++ src/App.js
@@ -0,0 +1,25 @@
+// @flow
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome to Tweets Around Me!</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+})
